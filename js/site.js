a2ed7ff07899d8a303f57833432ad965fdf9f195
FaridBAHR1 <f4ridb4hri@gmail.com>
2023-05-16 11:31:55 +1100

    Rewind - Model View Controller

--- js/site.js
+++ js/site.js
@@ -1,4 +1,26 @@
-//Call function
-function Application_Main(){
-    alert("Main Function");
+//Get String's Value - Controller/
+function getValue(){
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+}
+
+//Reverse String - Model/Logic
+function reverseString(userString){
+    let revString = [];
+    let name = namy;
+    name[0] = "n";
+    name[3] = "y";
+
+    //reverse using forLoop
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        for (let index = 0)
+    }
+}
+
+//Display Revered String - View/Display
+function displayString(){
+    
 }
